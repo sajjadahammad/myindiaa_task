db25e296733ff715cc5a8697689ec61f2d0a5d2c
sajjadahammed <sajjad08@protonmail.com>
2024-07-01 12:02:19 +0530

    fix2

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
         theme_color: '#000000',
         icons: [
           {
-            src: "public/assets/icons/manifest-icon-192.maskable.png",
+            src: "assets/icons/manifest-icon-192.maskable.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any"
