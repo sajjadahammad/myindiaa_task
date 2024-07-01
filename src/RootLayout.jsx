bcf6d753b137895a3c22ef8075330e39a5297536
sajjadahammed <sajjad08@protonmail.com>
2024-07-01 14:57:20 +0530

    err fix

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "./components/Navbar"
+import { ScrollRestoration } from "react-router-dom";
 
 export default function RootLayout() {
   return (
     <div className="max-w-screen-xl mx-auto font-poppins">
+        <ScrollRestoration/>
         <Navbar/>
         <div className="px-4 lg:px-0 py-4">
         <Outlet/>
