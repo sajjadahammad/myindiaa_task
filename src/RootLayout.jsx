220f8efcfe8190d4708edea61cf3abd3ad83b2de
sajjadahammed <sajjad08@protonmail.com>
2024-07-06 12:46:58 +0530

    analytics

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import { ScrollRestoration } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout() {
   return (
     <div className="max-w-screen-xl mx-auto font-poppins">
         <ScrollRestoration/>
         <Navbar/>
+        <Analytics/>
         <div className="px-4 lg:px-0 py-4">
         <Outlet/>
         </div>
